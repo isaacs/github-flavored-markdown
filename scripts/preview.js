2240bc433155fdc01d0cd4753b1759adeecf55a5
tekkub <tekkub@gmail.com>
2009-11-25 00:56:33 -0700

    Add underscore examples

--- scripts/preview.js
+++ scripts/preview.js
@@ -7,7 +7,8 @@ $(function() {
     $("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n"));
   });
 
-  var sample = "#### Autolinking\na non-markdown link: http://github.com/blog\nthis one is [a markdown link](http://github.com/blog)\nEmail test: support@github.com\n\n\
+  var sample = "#### Underscores\nthis should have _emphasis_\nthis_should_not\n_nor_should_this\n\n\
+#### Autolinking\na non-markdown link: http://github.com/blog\nthis one is [a markdown link](http://github.com/blog)\nEmail test: support@github.com\n\n\
 #### Commit links\nc4149e7bac80fcd1295060125670e78d3f15bf2e\ntekkub@c4149e7bac80fcd1295060125670e78d3f15bf2e\nmojombo/god@c4149e7bac80fcd1295060125670e78d3f15bf2e\n\n\
 #### Issue links\nissue #1\ntekkub#1\nmojombo/god#1";
   $("#user_input").text(sample);
